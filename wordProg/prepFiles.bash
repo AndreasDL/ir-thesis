207aa447af4abebf3066b8a5cf076a9f3078e5f7
andreasDL <andreasdelille@hotmail.Com>
2015-08-28 17:04:34 +0200

    exercises

---
+++ wordProg/prepFiles.bash
@@ -0,0 +1,23 @@
+if [ ! -d "/home/drew/done/megaTest" ]; then
+	exit;
+fi
+
+cd /home/drew/done/megaTest;
+
+# extract all
+find -iname "*.epub" -exec 7z x {} -aoa \;
+
+# cleanup other files than .xhtml
+mkdir /tmp/files;
+mv *.xhtml /tmp/files;
+rm -rvf *;
+mv /tmp/files/* .;
+rmdir /tmp/files;
+
+#cleanup in files
+#html tags
+find -iname '*.xhtml' -exec sed -i -e 's/<[^>]*>//g' {} \;
+#other chars
+find -iname '*.xhtml' -exec sed -i -e 's/[^a-zA-Z0-9 ]//g' {} \;
+
+cd -;
