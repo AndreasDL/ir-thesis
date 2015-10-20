89576f26b6ff89fb772c76f069bf56056f9adb3b
andreasDL <andreasdelille@hotmail.Com>
2015-10-20 15:28:32 +0200

    singlepersondata

--- app/dataLoader.py
+++ app/dataLoader.py
@@ -5,16 +5,20 @@ import featureExtractor as FE
 
 from pprint import pprint
 
-def loadData(pad, startFileIndex, fileCount):
-	features = np.zeros((40, 1)) #40 x 1 feature
-	labels = np.zeros((40, 1)) #40 x 1 label
 
-	#load testSet
-	for i in range(fileCount):
-		fname = 'dataset/s'
-		if i+1+startFileIndex < 10:
+
+def loadMultiPersonData(train_fileCount, test_fileCount, pad='dataset/s'):
+	x_train = np.zeros((40, 1)) #40 x 1 feature
+	y_train = np.zeros((40, 1)) #40 x 1 label
+	x_test  = np.zeros((40, 1)) #40 x 1 feature
+	y_test  = np.zeros((40, 1)) #40 x 1 label
+
+	#load trainSet
+	for i in range(train_fileCount):
+		fname = pad
+		if i+1 < 10:
 			fname += '0' 
-		fname += str(i+1+startFileIndex) + '.dat'
+		fname += str(i+1) + '.dat'
 		
 		with open(fname,'rb') as f:
 			p = pickle._Unpickler(f)
@@ -24,16 +28,57 @@ def loadData(pad, startFileIndex, fileCount):
 			#data['labels'][video , attribute]
 			#data['data'][video, channel, value]
 
-			labels = data['labels'][:,1] #only valence needed
+			y_train = data['labels'][:,1] #only valence needed
 			#structure y_train[video]
 
-			#get features
-			#different left/right right more active than left
-			#calculate sum_left & sum_right & sum_total = sum_left + sum_right
-			#use feature= sum_right / sum_total
-			#don't use sum(x['data']), cuz we ignore center electrodes
+			for j in range(len(data['data'])): #for each video
+				x_train[j] = FE.calculateFeatures(data['data'][j])
+
+	#load testSet
+	for i in range(test_fileCount):
+		fname = pad
+		if i+1+train_fileCount < 10:
+			fname += '0' 
+		fname += str(i+1+train_fileCount) + '.dat'
+		
+		with open(fname,'rb') as f:
+			p = pickle._Unpickler(f)
+			p.encoding= ('latin1')
+			data = p.load()
+			#structure of data element:
+			#data['labels'][video , attribute]
+			#data['data'][video, channel, value]
+
+			y_test = data['labels'][:,1] #only valence needed
 
 			for j in range(len(data['data'])): #for each video
-				features[j] = FE.calculateFeatures(data['data'][j])
+				x_test[j] = FE.calculateFeatures(data['data'][j])
 
-	return [features, labels]
+	return [x_train, y_train, x_test, y_test]
+
+def loadSinglePersonData(trainVideoCount, pad='dataset/s'):
+	x_train = np.zeros((trainVideoCount, 1)) # .. x 1 feature
+	y_train = np.zeros((trainVideoCount, 1)) # .. x 1 label
+	x_test  = np.zeros((40-trainVideoCount, 1)) # .. x 1 feature
+	y_test  = np.zeros((40-trainVideoCount, 1)) # .. x 1 label
+
+	fname = str(pad) +'01.dat'
+	with open(fname,'rb') as f:
+		p = pickle._Unpickler(f)
+		p.encoding= ('latin1')
+		data = p.load()
+		#structure of data element:
+		#data['labels'][video , attribute]
+		#data['data'][video, channel, value]
+
+		y_train = data['labels'][0:trainVideoCount,1] #only valence needed
+		y_test  = data['labels'][trainVideoCount:,1]
+
+		#split single person in test and train set
+		for j in range(trainVideoCount): #for each video
+			x_train[j] = FE.calculateFeatures(data['data'][j])
+		
+		for j in range(len(data['data']) - trainVideoCount): #for each video
+			x_test[j] = FE.calculateFeatures(data['data'][j])
+
+	return [x_train, y_train, x_test, y_test]
--- app/main.py
+++ app/main.py
@@ -7,18 +7,18 @@ import numpy as np
 from pprint import pprint
 
 if __name__ == "__main__":
-	trainPersons = 5 # one set = one person
-	testPersons = 1 # one set = one person
+	trainVideos = 30
 
 	#load trainSet
 	#y_train: holds all valence values for each movie
 	#x_train: holds all features for each movie
-	(x_train, y_train) = DL.loadData('dataset/s', 0, trainPersons)
+	#similar for test set
+	(x_train, y_train, x_test, y_test) = DL.loadSinglePersonData(trainVideos)
 
-	#load testSet
-	#y_test: holds all the valence values for each movie
-	#x_test: holds all features for each movie
-	(x_test, y_test) = DL.loadData('dataset/s', trainPersons, testPersons)	
+	pprint(x_train)
+	pprint(y_train)
+	pprint(x_test)
+	pprint(y_test)
 
 	#models.linReg(x_train,y_train,x_test,y_test)
 	models.linReg(x_train,y_train,x_test,y_test)
