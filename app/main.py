6d9093512995c387fa53a6f51292039bd7fd6913
andreasDL <andreasdelille@hotmail.Com>
2015-11-06 22:13:54 +0100

    cleanup final

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ import plotters
 
 if __name__ == "__main__":
 	test_size = 4
+	used_person = 7
 
 	#load trainSet
 	#y_train: holds all valence values for each movie
 	#x_train: holds all features for each movie
 	#similar for test set
-	(X_train, y_train, X_test, y_test) = DL.loadSinglePersonData(person=7, test_size=test_size)
+	(X_train, y_train, X_test, y_test) = DL.loadSinglePersonData(person=used_person, test_size=test_size)
 
 	#linear regression
 	train_err, test_err, regr = models.linReg(X_train,y_train,X_test,y_test)
