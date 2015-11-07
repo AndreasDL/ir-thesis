22c4f3af6b15be731d8949a24c2fd58333a46356
andreasDL <andreasdelille@hotmail.Com>
2015-11-07 18:25:36 +0100

    file dump and load

--- app/dataLoader.py
+++ app/dataLoader.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import numpy as np
 from featureExtractor import extract
@@ -23,7 +24,7 @@ def loadSinglePersonData(person, test_size=4, pad='../dataset', happyThres=0.5):
 
 		y = np.array( data['labels'][:,0] ) #ATM only valence needed
 		y = (y - 1) / 8 #1->9 to 0->8 to 0->1
-		y[ y < happyThres ] = 0
+		y[ y <= happyThres ] = 0
 		y[ y > happyThres ] = 1
 
 		#extract features
@@ -34,3 +35,70 @@ def loadSinglePersonData(person, test_size=4, pad='../dataset', happyThres=0.5):
 	X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size, random_state=42)
 
 	return [np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)]
+
+def loadMultiplePersonsData(personCount=32, test_size=8, pad='../dataset', happyThres=0.5):
+	#loads data for all persons, for each person put part of videos in test set
+	#create two classes happy and not happy
+	
+	X_train, y_train = [], []
+	X_test , y_test  = [], []
+
+	for person in range(1,personCount+1):
+		fname = str(pad) + '/s'
+		if person < 10:
+			fname += '0'
+		fname += str(person) + '.dat'
+		with open(fname,'rb') as f:
+			p = pickle._Unpickler(f)
+			p.encoding= ('latin1')
+			data = p.load()
+			#structure of data element:
+			#data['labels'][video] = [valence, arousal, dominance, liking]
+			#data['data'][video][channel] = [samples * 8064]
+
+			y = np.array( data['labels'][:,0] ) #ATM only valence needed
+			y = (y - 1) / 8 #1->9 to 0->1
+			y[ y <= happyThres ] = 0
+			y[ y > happyThres ] = 1
+
+			#extract features
+			X = []
+			for j in range(len(data['data'])): #for each video
+				X.append( extract(data['data'][j]) )
+
+		#split into train and test set, while shuffeling
+		X_tr, X_te, y_tr, y_te = train_test_split(X,y,test_size=test_size, random_state=42)
+
+		#add to list
+		X_train.extend(X_tr)
+		X_test.extend(X_te)
+		
+		y_train.extend(y_tr)
+		y_test.extend(y_te)
+
+
+	return [np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)]
+
+def dump(X_train, y_train, X_test, y_test, name, path='../dumpedData'):
+	fname = path + '/' + name
+	data = { 'X_train': X_train, 
+		'y_train': y_train,
+		'X_test': X_test,
+		'y_test': y_test 
+	}
+	with open(fname, 'wb') as f:
+		pickle.dump( data, f )
+
+def load(name, path='../dumpedData'):
+	fname = path + '/' + name
+
+	data = None
+	with open(fname,'rb') as f:
+		p = pickle._Unpickler(f)
+		p.encoding= ('latin1')
+		data = p.load()
+	if data == None:
+		print('data loading failed for file:', fname)
+		exit(-1)
+
+	return data['X_train'], data['y_train'], data['X_test'], data['y_test']
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import dataLoader as DL
 import models
 import plotters
+import numpy as np
 
 def main_single():
 	test_size = 4
@@ -17,15 +18,18 @@ def main_single():
 		'\n\tTrain accuracy: ', train_acc,
 		'\n\tTest accuracy: ' , test_acc
 	)
+def main_all_one_by_one():
+	test_size = 8
+	avg_train, avg_test = 0, 0
 
-def main_All():
-	test_size = 4
-	for person in range(1,32):
+	for person in range(1,33):
 		#load dataset
 		(X_train, y_train, X_test, y_test) = DL.loadSinglePersonData(person=person, test_size=test_size)
 
 		#classify
 		train_acc, test_acc, clf = models.linSVM(X_train,y_train, X_test,y_test)
+		avg_test  += test_acc
+		avg_train += train_acc
 
 		#scores
 		print('Person: ', person,
@@ -34,5 +38,34 @@ def main_All():
 			'\tTest accuracy: ' , test_acc
 		)
 
+	avg_test  /= 32
+	avg_train /= 32
+
+	print('avg\t',
+		'\tTrain accuracy: ', train_acc,
+		'\tTest accuracy: ' , test_acc
+	)
+def main_all():
+	test_size = 4
+
+	#load dataset
+	print('loading')
+	#quick load 
+	#(X_train, y_train, X_test, y_test) = DL.load('allpersonsTS4LMRClass')
+	
+	#slow load + dump
+	(X_train, y_train, X_test, y_test) = DL.loadMultiplePersonsData(test_size=test_size)
+	DL.dump(X_train, y_train, X_test, y_test, 'allpersonsTS4LMRClass')
+
+	#classify
+	print('training')
+	train_acc, test_acc, clf = models.linSVM(X_train,y_train, X_test,y_test)
+
+	#scores
+	print('model: lin SVM',
+		'\n\tTrain accuracy: ', train_acc,
+		'\n\tTest accuracy: ' , test_acc
+	)
+
 if __name__ == "__main__":
-	main_All()
+	main_all()
