b7bb24c8005da79be7f522070ae022fc122fa013
andreasDL <andreasdelille@hotmail.Com>
2015-11-19 00:15:35 +0100

    multithreading per person

--- app/main.py
+++ app/main.py
@@ -5,51 +5,42 @@ from multiprocessing import Pool
 
 left_channels_to_try  = ['Fp1', 'AF3', 'F3', 'F7', 'FC5', 'FC1', 'C3', 'T7', 'CP5', 'CP1', 'P3', 'P7', 'PO3']
 right_channels_to_try = ['Fp2', 'AF4', 'F4', 'F8', 'FC6', 'FC2', 'C4', 'T8', 'CP6', 'CP2', 'P4', 'P8', 'PO4']
-testVideos = 8
-classCount = 2
 
-def channelPairWorker(index):
-    left  = left_channels_to_try[index]
-    right = right_channels_to_try[index]
-    print("running channel pair: ", left, " - ", right)
+#generate the extraction function
+def func(samples):
+    features = []
+    for left, right in zip(left_channels_to_try, right_channels_to_try):
+        features.extend( FE.LMinRFraction(samples, left_channel=left, right_channel=right) )
 
-    avg_test, avg_train = 0, 0
+    return features
 
-    #generate the extraction function
-    def func(samples):
-        return FE.LMinRFraction(samples, left_channel=left, right_channel=right)
 
-    for person in range(1,33):
-        #load dataset
-        (X_train, y_train, X_test, y_test) = DL.loadSinglePersonData(classCount=classCount, testVideos=testVideos, featureFunc=func, person=person)
+def personWorker(person):
+    print('working on person ', person)
 
-        #classify
-        train_acc, test_acc, clf = models.linSVM(X_train,y_train, X_test,y_test)
-        avg_test  += test_acc
-        avg_train += train_acc
+    testVideos = 8
+    classCount = 2
 
-        #scores for one person
-        #print('Person: ', person,
-        #    '\tmodel: lin SVM',
-        #    '\tTrain accuracy: ', train_acc,
-        #    '\tTest accuracy: ' , test_acc
-        #)
+    #load dataset
+    (X_train, y_train, X_test, y_test) = DL.loadSinglePersonData(
+        classCount=classCount,
+        testVideos=testVideos,
+        featureFunc=func,
+        person=person
+    )
 
-    avg_test  /= 32
-    avg_train /= 32
+    #classify
+    train_acc, test_acc, clf = models.linSVM(X_train,y_train, X_test,y_test)
 
     return test_acc
 
 
 def main_all_one_by_one():
-    f = open('output-noborder.txt', 'w')
-    f.write('testVideos: ' + str(testVideos) + ' classCount: ' + str(classCount) + "\n")
+    pool = Pool(processes=4)
+    results = pool.map( personWorker, range(1,33) )
 
-    pool = Pool(processes=5)
-    results = pool.map( channelPairWorker, range(len(left_channels_to_try)) )
-    
-    for i, result in enumerate(results):
-    	print(left_channels_to_try[i], ' - ', right_channels_to_try[i], ' -> ', result)
+    print('test results: ', np.mean(np.array(results)))
+
 
 if __name__ == "__main__":
     main_all_one_by_one()
