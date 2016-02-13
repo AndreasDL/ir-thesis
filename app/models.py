9ab0461b0a6b53427badf0809ee56a1aabea93cb
Andreas De Lille <andreasdelille@hotmail.com>
2016-02-13 12:53:46 +0100

    models + reporters complete

---
+++ app/models.py
@@ -0,0 +1,110 @@
+import personLoader
+import featureExtractor
+import classificators
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import StratifiedShuffleSplit, KFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+class AModel:
+    def __init__(self,personLoader,reporter):
+        self.personLoader = personLoader
+        self.reporter = reporter
+
+    def run(self):
+        return None;
+
+class stdModel(AModel):
+
+    def __init__(self,personLoader, reporter,max_k):
+        AModel.__init__(self,personLoader,reporter)
+        self.max_k = max_k
+
+    def run(self,person):
+        #load data
+        X_train, y_train, X_test, y_test = self.personLoader.load(person)
+
+        #init academic loop to optimize k param
+        k = 1
+        anova_filter = SelectKBest(f_regression)
+        lda          = LinearDiscriminantAnalysis()
+        anova_lda    = Pipeline([
+            ('anova', anova_filter),
+            ('lda', lda)
+        ])
+        anova_lda.set_params(anova__k=k)
+
+        K_CV = KFold(n=len(X_train),
+            n_folds=len(X_train),
+            random_state=17, #fixed randomseed ensure that the sets are always the same
+            shuffle=False
+        ) #leave out one validation
+
+        predictions, truths = [], []
+        for train_index, CV_index in K_CV: #train index here is a part of the train set
+            #train
+            anova_lda.fit(X_train[train_index], y_train[train_index])
+
+            #predict
+            pred = anova_lda.predict(X_train[CV_index])
+
+            #save for metric calculations
+            predictions.extend(pred)
+            truths.extend(y_train[CV_index])
+
+        #optimization metric:
+        best_acc = self.reporter.optMetric(predictions,truths)
+        best_k   = k
+
+        #now try different k values
+        for k in range(2,self.max_k):
+            anova_filter = SelectKBest(f_regression)
+            lda          = LinearDiscriminantAnalysis()
+            anova_lda    = Pipeline([
+                ('anova', anova_filter),
+                ('lda', lda)
+            ])
+            #set k param
+            anova_lda.set_params(anova__k=k)
+
+            #leave one out validation to determine how good the k value performs
+            K_CV = KFold(n=len(X_train),
+                n_folds=len(X_train),
+                random_state=17, #fixed randomseed ensure that the sets are always the same
+                shuffle=False
+            )
+
+            predictions, truths = [], []
+            for train_index, CV_index in K_CV: #train index here is a part of the train set
+                #train
+                anova_lda.fit(X_train[train_index], y_train[train_index])
+
+                #predict
+                pred = anova_lda.predict(X_train[CV_index])
+
+                #save for metric calculations
+                predictions.extend(pred)
+                truths.extend(y_train[CV_index])
+
+            #optimization metric:
+            curr_acc = self.reporter.optMetric(predictions, truths)
+            if curr_acc > best_acc:
+                best_acc = curr_acc
+                best_k   = k
+
+        #now the k param is optimized and stored in best_k
+
+        #create classifier and train it on all train data
+        anova_filter = SelectKBest(f_regression)
+        lda          = LinearDiscriminantAnalysis()
+        anova_lda    = Pipeline([
+            ('anova', anova_filter),
+            ('lda', lda)
+        ])
+        #set k param
+        anova_lda.set_params(anova__k=best_k)
+        anova_lda.fit(X_train, y_train)
+
+        predictions = anova_lda.predict(X_test)
+
+        self.reporter.genReport(person,predictions,truths,anova_lda)
---
+++ app/reporters.py
@@ -0,0 +1,156 @@
+import numpy as np
+from sklearn.metrics import roc_auc_score
+import datetime
+import time
+
+class AReporter:
+    def __init__(self,featureExtractor):
+        self.featExtr = featureExtractor #needed to get the feature names
+
+    def optMetric(self,predictions,truths):
+        return None
+
+    def genReport(self,person,predictions,truths,fpad='../results/'):
+        return None
+
+
+class CSVReporter(AReporter):
+    def __init__(self,featureExtractor):
+        AReporter(self,featureExtractor)
+
+    def optMetric(self,predictions,truths):
+        return self.accuracy(predictions,truths)
+
+    def genReport(self,person,predictions,truths,anova_out,fpad='../results/'):
+        acc  = self.accuracy(predictions, truths)
+        (tpr,tnr,fpr,fnr) = self.tprtnrfprfnr(predictions, truths)
+        auc = self.auc(predictions, truths)
+
+        used_features = anova_out.named_steps['anova'].get_support()
+
+        best_k = 0
+        for bool in used_features:
+            if bool
+                best_k += 1
+
+        #print to console
+        print('person: ', person,
+            ' - k: '  , str(best_k),
+            ' - acc: ', str(acc),
+            ' - tpr: ' , str(tpr),
+            ' - tnr: ' , str(tnr),
+            ' - auc: ', str(auc),
+            'used features', anova_out.named_steps['anova'].get_support()
+        )
+
+        #output to file
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H%M%S')
+        f = open(fpad + "output" + str(st) + ".txt", 'w')
+        f.write('person;best_k;acc;tpr,tnr,fpr,fnr,auc\n')
+
+        s = str(person+1) + ';' +\
+            str(best_k) + ';' +\
+            str(acc) + ';' +\
+            str(tpr) + ';' + str(tnr) + ';' +\
+            str(fpr) + ';' + str(fnr) + ';' +\
+            str(auc) + ';'
+
+        for bool in used_features:
+            if bool:
+                s += 'X'
+            s+= ';'
+
+        f.write(s)
+        f.write('\n')
+
+
+        f.write('\nmedian;')
+        for column in range(len(perf_results[0])):
+            f.write(str(np.median(perf_results[:,column])) + ';')
+
+        f.write('\navg;')
+        for column in range(len(perf_results[0])):
+            f.write(str(np.average(perf_results[:,column])) + ';')
+
+        f.write('\nstd;')
+        for column in range(len(perf_results[0])):
+            f.write(str(np.std(perf_results[:,column])) + ';')
+
+
+        f.close()
+
+    def accuracy(self,predictions, truths):
+        acc = 0
+        for pred, truth in zip(predictions, truths):
+            acc += (pred == truth)
+
+        return acc / float(len(predictions))
+
+    def tprtnrfprfnr(self,predictions, truths):
+        tp, tn = 0, 0
+        fp, fn = 0, 0
+
+        pos_count = np.sum(truths)
+        neg_count = len(truths) - pos_count
+
+        for pred, truth in zip(predictions, truths):
+            if pred == truth: #prediction is true
+                if pred == 1:
+                    tp += 1
+                else:
+                    tn += 1
+
+            else:
+                if pred == 1:
+                    fp += 1
+                else:
+                    fn += 1
+
+        return tp / float(pos_count), tn / float(neg_count), fp / float(pos_count), fn / float(neg_count)
+
+    def auc(self,predictions, truths):
+
+        return roc_auc_score(truths, predictions)
+
+    def classCorrect(self,predictions, truths):
+        classCount = [0,0,0,0]
+        for i in range(4):
+            classCount[i] = np.sum(truths[truths == i])
+
+        classPreds = [0,0,0,0]
+        for pred, truth in zip(predictions, truths):
+            if pred == truth:
+                classPreds[int(truth)] += 1
+
+        return np.array( np.array(classPreds) / np.array(classCount) )
+
+    def dimCorrect(self,predictions, truths):
+        #assign classes
+        #              | low valence | high valence |
+        #low  arrousal |      0      |       2      |
+        #high arrousal |      1      |       3      |
+
+        #dimCount = | low arr Count | high arr count | low valence count | high valence count |
+        dimCount = [0,0,0,0]
+        for i in range(2):
+            #low (i==0) and high(i==1) arouousal
+            dimCount[i]   = np.sum(truths[truths == i]) + np.sum(truths[truths == i+2])
+
+            #low(i==0) and high (i==1) valence
+            dimCount[i+2] = np.sum(truths[truths == i]) + np.sum(truths[truths == i+1])
+
+        predCount = [0,0,0,0]
+        for pred, truth in zip(predictions, truths):
+            #arousal correct ?
+            if pred == truth or pred == truth + 2 or pred == truth -2:
+                #low (truth % 2 == 0) , high (truth % 2 == 1
+                predCount[int(truth) % 2] += 1
+
+            #valence
+            if pred == truth or pred == truth + 1 or pred == truth -1:
+                if truth == 0 or truth == 1:
+                    predCount[2] += 1
+                else:
+                    predCount[3] += 1
+
+        return np.array(predCount) / np.array(dimCount)µ
