62432b92558e14b603a1bdbb7538d15989ba7ecc
Andreas De Lille <andreasdelille@hotmail.com>
2016-04-06 13:43:42 +0200

    step 2 complete

--- app/main/RFSinglePers.py
+++ app/main/RFSinglePers.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 STOPPERSON = 3
 RUNS = 5
-N_ESTIMATORS = 2000
+N_ESTIMATORS = 100
 
 def getFeatures():
     # create the features
@@ -29,6 +29,7 @@ def getFeatures():
     featExtr.addFE(FE.STDInterBeatExtractor())
 
     #EEG
+    '''
     for channel in FE.all_EEG_channels:
 
         featExtr.addFE(
@@ -97,7 +98,7 @@ def getFeatures():
                     featName='RCAU ' + FE.all_channels[front] + ',' + FE.all_channels[post]
                 )
             )
-
+    '''
     return featExtr
 def genPlot(importances, std, fpad="../../results/plots/"):
 
@@ -119,7 +120,9 @@ def genPlot(importances, std, fpad="../../results/plots/"):
     plt.savefig(fname)
     plt.clf()
 
-def step1(X,y, features, runs=RUNS,n_estimators=N_ESTIMATORS, criterion='gini'):
+def step1(X,y, featureNames, runs=RUNS,n_estimators=N_ESTIMATORS, criterion='gini'):
+    print('step1')
+
     # grow forest
     forest = RandomForestClassifier(
         n_estimators=n_estimators,
@@ -143,37 +146,87 @@ def step1(X,y, features, runs=RUNS,n_estimators=N_ESTIMATORS, criterion='gini'):
 
     # genPlot(importances, stds)
 
-
     #get threshold from cart estimator
     cart = DecisionTreeClassifier(criterion=criterion)
-    importances = []
+    cart_importances = []
     for i in range(runs):
         cart.fit(X, y)
-        importances.append(cart.feature_importances_)
-    std = np.std(importances, axis=0)
-    # importances = np.average(importances, axis=0)
-    thresh = float(np.amin(std))
+        cart_importances.append(cart.feature_importances_)
+    thresh = float(np.amin(np.std(cart_importances, axis=0)))
 
     #throw out everything < threshold
     indices_to_keep = []
     for index, (imp, std) in enumerate(zip(importances, stds)):
-        if float(imp) - float(std) > thresh: #worst case filtering
+        if imp - std > thresh: #worst case filtering
             indices_to_keep.append(index)
+    indices_to_keep = np.array(indices_to_keep)
 
+    #sort indices to keep
+    importances = importances[indices_to_keep]
+    featureNames = featureNames[indices_to_keep]
+
+    # sort features
+    indices_to_keep = np.array(np.argsort(importances)[::-1])
+    featureNames = featureNames[indices_to_keep]
+
+    return np.array(indices_to_keep), featureNames
+def step2(X,y, featureNames, runs=RUNS,n_estimators=N_ESTIMATORS, criterion='gini'):
+    print('step2')
+    featuresLeft = len(X[0])
+
+    #for featCount = 1 ~> remaining indices
+    oob_scores = []
+    for featCount in range(1,featuresLeft + 1):
+        print(featCount)
+        run_errors = []
+        forest = RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_features='auto',
+            criterion=criterion,
+            n_jobs=-1,
+            oob_score=True,
+            bootstrap=True
+        )
+        X_temp = X[:,:featCount]
+        for i in range(runs):
+            forest.fit(X_temp,y)
+            run_errors.append(forest.oob_score_)
+
+        oob_scores.append(run_errors)
+
+    #std
+    stds = np.std(oob_scores, axis=1)
+    avgs = np.average(oob_scores, axis=1)
+
+    #genPlot(avgs,stds)
+
+    highest_avg_index = np.argmax(avgs)
+    highest_avg       = avgs[highest_avg_index]
+    highest_std       = stds[highest_avg_index]
+
+    #look for smaller kandidates, keeping track of STD
+    for i in range(highest_avg_index):
+        if avgs[i] - stds[i] > highest_avg - highest_std:
+            highest_avg_index = i
+            highest_std = stds[i]
+            highest_avg = avgs[i]
+
+    return highest_avg_index
 
 
 def RFPerson(person):
     #load X , y
     # load all features & keep them in memory
+    featExtr = getFeatures()
+    featureNames = np.array(featExtr.getFeatureNames())
+
     y_cont = load('cont_y_p' + str(person))
-    featExtr = getFeatures() #needed later on
     if y_cont == None:
         print('[Warn] Rebuilding cache -  person ' + str(person))
-        X, y_cont = personLoader.PersonsLoader(
+        X, y_cont = personLoader.NoTestsetLoader(
             classificator=Classificators.ContValenceClassificator(),
             featExtractor=featExtr,
-            stopPerson=STOPPERSON,
-        ).load()
+        ).load(person)
 
         dump(X, 'X_p' + str(person))
         dump(y_cont, 'cont_y_p' + str(person))
@@ -189,12 +242,29 @@ def RFPerson(person):
     X = np.true_divide(X, np.std(X, axis=0))
 
     #step 1 determine importances using RF forest
-    step1(X,y_disc,featExtr.getFeatureNames())
+    indices_step1 = load('indices')
+    featureNames_step1  = load('featNames_step1')
+    if indices_step1 == None or featureNames == None:
+        indices_step1, featureNames_step1 = step1(X,y_disc,featureNames)
+        dump(indices_step1, 'indices')
+        dump(featureNames_step1, 'featNames_step1')
+
+    featureNames = np.array(featureNames_step1)
+    indices = np.array(indices_step1)
+
+    #filter features (X) based on the results from step 1
+    X = X[:,indices]
+
+    #step 2
+    featCount_step2 = step2(X,y_disc,featureNames_step1)
+    indices = indices[:featCount_step2]
+    X = X[:,indices]
+    featureNames = featureNames[indices]
 
 
-    print('hoi')
 
 
 
 if __name__ == '__main__':
+    #TODO for person in range(STOPPERSON):
     RFPerson(1)
