440e357aead1e1ae76b32c6dbef9ffebbd2fea39
Andreas De Lille <andreasdelille@hotmail.com>
2016-04-15 21:33:50 +0200

    nightscript running

--- app/main/nightRun.py
+++ app/main/nightRun.py
@@ -1,8 +1,12 @@
-from .RFPers import RFPers
-from .SVMPers import SVMPers
 from Classificators import ContArousalClassificator, ContValenceClassificator
+from main.PERS.RFPers import RFPers
+from main.PERS.SVMPers import SVMPers
 
 for model in [RFPers, SVMPers]:
     for feat in ['ALL','PHY','EEG']:
         for cls in [ContArousalClassificator(), ContValenceClassificator()]:
-            model(feat,32,40,cls).run()
+            try:
+                model(feat,32,40,cls).run()
+            except Exception as e:
+                print(model, feat, cls, 'went wrong')
+                print(e)
