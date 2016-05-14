25ef52f4660bdf0a98f58e65cc8cad8871c37106
Andreas De Lille <andreasdelille@hotmail.com>
2016-05-14 11:37:47 +0200

    h1 en h2 rework, TODO figures :1234:

--- app/jaccard.py
+++ app/jaccard.py
@@ -4,50 +4,65 @@ import numpy as np
 ddpath = '../dumpedData/'
 
 trees = 2000
-for dim in ['valence','arousal']:
-    for featSet in ['all']:
-        for runs in [1,5,10,20,30,40,50]:
-            #open first & second trial
-            ppath  = ddpath
-            ppath += 'persScript_run0_' + str(trees) + '_' + str(runs) + '/'
-            ppath += dim + '/'
-            ppath += featSet + '/results/finFeatSVMLIN.csv'
+for setting in ['pers','gen']:
+    print(setting)
 
-            npath  = ddpath
-            npath += 'persScript_run1_' + str(trees) + '_' + str(runs) + '/'
-            npath += dim + '/'
-            npath += featSet + '/results/finFeatSVMLIN.csv'
+    for dim in ['valence','arousal']:
+        for featSet in ['all']:
+            for runs in [1,5,10,20,30,40,50]:
+                #open first & second trial
+                ppath  = ddpath
+                ppath += setting
+                ppath += 'Script_run0_' + str(trees) + '_' + str(runs) + '/'
+                ppath += dim + '/'
+                ppath += featSet + '/results/finFeatSVM'
+                if setting == 'pers':
+                    ppath += 'LIN'
+                else:
+                    ppath += 'RBF'
+                ppath += '.csv'
 
-            pfile = open(ppath, 'r')
-            nfile = open(npath, 'r')
+                npath  = ddpath
+                npath += setting
+                npath += 'Script_run1_' + str(trees) + '_' + str(runs) + '/'
+                npath += dim + '/'
+                npath += featSet + '/results/finFeatSVM'
+                if setting == 'pers':
+                    npath += 'LIN'
+                else:
+                    npath += 'RBF'
+                npath += '.csv'
 
-            jaccards = []
-            for person, (pline, nline) in enumerate(zip(pfile,nfile)):
-                if person > 1 and person < 34:
-                    pfeats = pline.strip('\n').split(';')
-                    nfeats = nline.strip('\n').split(';')
+                pfile = open(ppath, 'r')
+                nfile = open(npath, 'r')
 
-                    #fix length issues
-                    if len(pfeats) < len(nfeats):
-                        diff = len(nfeats) - len(pfeats)
-                        for i in range(diff):
-                            pfeats.append('')
-                    elif len(nfeats) < len(pfeats):
-                        diff = len(pfeats) - len(nfeats)
-                        for i in range(diff):
-                            nfeats.append('')
+                jaccards = []
+                for person, (pline, nline) in enumerate(zip(pfile,nfile)):
+                    if person > 1 and person < 34:
+                        pfeats = pline.strip('\n').split(';')
+                        nfeats = nline.strip('\n').split(';')
 
-                    #jaccard
-                    jackie = jaccard_similarity_score(pfeats, nfeats)
+                        #fix length issues
+                        if len(pfeats) < len(nfeats):
+                            diff = len(nfeats) - len(pfeats)
+                            for i in range(diff):
+                                pfeats.append('')
+                        elif len(nfeats) < len(pfeats):
+                            diff = len(pfeats) - len(nfeats)
+                            for i in range(diff):
+                                nfeats.append('')
 
-                    #prints
-                    #print('\t' + pline, end='')
-                    #print('\t' + nline, end='')
-                    #print('\t' + str(jackie))
+                        #jaccard
+                        jackie = jaccard_similarity_score(pfeats, nfeats)
 
-                    jaccards.append(jackie)
+                        #prints
+                        #print('\t' + pline, end='')
+                        #print('\t' + nline, end='')
+                        #print('\t' + str(jackie))
 
-            pfile.close()
-            nfile.close()
+                        jaccards.append(jackie)
 
-            print(dim + ';' + featSet + ';' + str(runs) + ';' + str(np.average(jaccards)) + ';' + str(np.std(jaccards)))
+                pfile.close()
+                nfile.close()
+
+                print("\t" + dim + ';' + featSet + ';' + str(runs) + ';' + str(np.average(jaccards)) + ';' + str(np.std(jaccards)))
